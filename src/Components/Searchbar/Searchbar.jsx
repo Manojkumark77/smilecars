130f5ef316336aa9ae08a423357911ee5d89ee81
Karuppasamy <mkarup110@apac.comcast.com>
2025-05-07 15:27:32 +0530

    Change smilecars

---
+++ src/Components/Searchbar/Searchbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import "./Searchbar.css"
+
+
+const SearchBar = () => {
+  return (
+    <section className="search-bar">
+      <div className="search-item">
+        <label>&#128205; Location</label>
+        <input type="text" placeholder="Search your location" />
+      </div>
+      <div className="search-item">
+        <label>&#128197; Pickup Date</label>
+        <input type="datetime-local" />
+      </div>
+      <div className="search-item">
+        <label>&#128197; Return Date</label>
+        <input type="datetime-local" />
+      </div>
+      <button className="search-btn">Search</button>
+    </section>
+  );
+};
+
+export default SearchBar;
